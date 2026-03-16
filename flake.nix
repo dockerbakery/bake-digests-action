b368a149c3b2dd95d11742114c318eca68dafee4
Socheat <github@socheat.dev>
2026-03-16 17:42:55 +0700

    Add fake.nix

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+    let
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [
+                (self: super: {
+                  # yarn = super.yarn.override {
+                  #   nodejs = super.nodejs_24;
+                  # };
+                  # yarn-berry = super.yarn.override {
+                  #   nodejs = super.nodejs_22;
+                  # };
+                  # pnpm = super.pnpm.override {
+                  #   nodejs = super.nodejs_22;
+                  # };
+                })
+              ];
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = [
+              pkgs.nodejs_24
+              # pkgs.yarn
+              # pkgs.yarn-berry
+              # pkgs.pnpm
+              pkgs.pnpm_8
+              # pkgs.pnpm_10
+
+            ];
+            shellHook = ''
+              echo "Welcome to your Node.js development environment!"
+              echo "Node.js $(node -v)"
+            '';
+          };
+        }
+      );
+    };
+}
